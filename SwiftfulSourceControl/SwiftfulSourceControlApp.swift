0e71f3bb71103369ddfb9937f0c72fd4b12b0161
Steve Clark <bukoba.steve@gmail.com>
2024-09-21 11:33:23 +1000

    Initial Commit

---
+++ SwiftfulSourceControl/SwiftfulSourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulSourceControlApp.swift
+//  SwiftfulSourceControl
+//
+//  Created by Stephen Clark on 21/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulSourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
